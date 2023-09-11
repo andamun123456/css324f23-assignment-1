845176a0f0c63429c7958ca1eec59cd15c8a409b
andamun123456 <144469632+andamun123456@users.noreply.github.com>
2023-09-12 01:56:31 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -6,4 +6,40 @@ def is_goal(s):
 
 def successors(s):
     x, y, z = s
-    return []
+    t=8-x
+    if t>0:
+        if y>0:
+            if y>t:
+                yield((8,y-t,z),t)
+            else
+                yield((x+y,0,z),y)
+        if z>0:
+            if z>t:
+                yield((8,y,z-t),t)
+            else
+                yield((x+z,y,0),z)
+    t=5-y
+    if t>0:
+        if x>0:
+            if x>t:
+                yield((x-t,5,z),t)
+            else
+                yield((0,y+x,z),x)
+        if z>0:
+            if z>t:
+                yield((x,5,z-t),t)
+            else
+                yield((x,y+z,0),z)
+    t=3-z
+    if t>0:
+        if x>0:
+            if x>t:
+                yield((x-t,y,3),t)
+            else
+                yield((0,y,z+x),x)
+        if y>0:
+            if y>t:
+                yield((x,y-t,3),t)
+            else
+                yield((x,0,z+y),y)
+    return [x,y,z]
